e2c92c440c21e09f72cc8d17ce138c6c94272484
Nicolae <nicolae.radu99@gmail.com>
2021-12-16 23:37:46 +0200

    typo

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,6 @@ CREATE DATABASE emotions;
 
 USE emotions;
 
-CREATE TABLE emotions (emotion_name VARCHAR(20),response VARCHAR(20),security_level VARCHAR(40));
+CREATE TABLE emotions (emotion_name VARCHAR(20),response VARCHAR(20),severity_level VARCHAR(40));
 INSERT INTO emotions VALUES ('anger','response','bad_significant_impact');
 INSERT INTO emotions VALUES ('happiness','response1','good_high_impact');
